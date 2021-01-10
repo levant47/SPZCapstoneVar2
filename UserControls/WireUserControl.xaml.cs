9b6114bf42ef5c3361d6682cd5ef85fd89940394
Anton Bodiak <abodiak@prometheusgroup.com>
2021-01-10 13:02:22 +0200

    Advanced wires geometry

--- UserControls/WireUserControl.xaml.cs
+++ UserControls/WireUserControl.xaml.cs
@@ -14,14 +14,36 @@ namespace SPZCapstoneVar2.UserControls
 
             _startPosition = startPosition;
 
-            RenderTransform = new TranslateTransform(startPosition.X, startPosition.Y);
+            Horiz1.RenderTransform = new TranslateTransform(startPosition.X, startPosition.Y);
+            Vert.RenderTransform = new TranslateTransform(startPosition.X, startPosition.Y);
+            Horiz2.RenderTransform = new TranslateTransform(startPosition.X, startPosition.Y);
             Canvas.SetLeft(this, startPosition.X);
             Canvas.SetTop(this, startPosition.Y);
         }
 
         public void PointTo(Point newPosition)
         {
-            RenderTransform = new ScaleTransform(newPosition.X - _startPosition.X, 1);
+            var connectionWidth = newPosition.X - _startPosition.X;
+            var connectionHeight = newPosition.Y - _startPosition.Y;
+
+            // the first horizontal line
+            Horiz1.RenderTransform = new ScaleTransform(connectionWidth / 2, 1);
+
+            // the vertical line
+            {
+                var transformGroup = new TransformGroup();
+                transformGroup.Children.Add(new TranslateTransform(connectionWidth / 2, 0));
+                transformGroup.Children.Add(new ScaleTransform(1, connectionHeight));
+                Vert.RenderTransform = transformGroup;
+            }
+
+            // the second horizontal line
+            {
+                var transformGroup = new TransformGroup();
+                transformGroup.Children.Add(new ScaleTransform(connectionWidth / 2, 1));
+                transformGroup.Children.Add(new TranslateTransform(connectionWidth / 2, connectionHeight));
+                Horiz2.RenderTransform = transformGroup;
+            }
         }
     }
 }
